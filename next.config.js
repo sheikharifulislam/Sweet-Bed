c535c0654c11b4f0dde3df216071aca5f7bdaf78
sheikh ariful <sheikharifulislam770@gmail.com>
2022-03-30 06:35:31 +0600

    add env file

--- next.config.js
+++ next.config.js
@@ -1,10 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: false,
-  env: {
-    DB_URL: 'mongodb+srv://Ariful:JEQOqikmllHK0EYf@cluster0.jrudo.mongodb.net/Bookit?retryWrites=true&w=majority'
-  } 
+  swcMinify: false,  
 }
 
 module.exports = nextConfig
